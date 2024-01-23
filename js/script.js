6817eb20e4da1604f8927acf64246be5c28653de
Mateus <mateusc.ramos@outlook.com>
2024-01-23 18:08:16 -0300

    feat: color, opacity and inset event listeners

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ class BoxShadowGenerator {
     blurReference,
     spreadRange,
     spreadReference,
+    shadowColor,
+    shadowColorReference,
+    boxColor,
+    boxColorReference,
+    opacityRange,
+    opacityReference,
+    insetCheckbox,
     previewBox,
     rule,
     webkitRule,
@@ -21,6 +28,13 @@ class BoxShadowGenerator {
     this.blurReference = blurReference;
     this.spreadRange = spreadRange;
     this.spreadReference = spreadReference;
+    this.shadowColor = shadowColor;
+    this.shadowColorReference = shadowColorReference;
+    this.boxColor = boxColor;
+    this.boxColorReference = boxColorReference;
+    this.opacityRange = opacityRange;
+    this.opacityReference = opacityReference;
+    this.insetCheckbox = insetCheckbox;
     this.previewBox = previewBox;
     this.rule = rule;
     this.webkitRule = webkitRule;
@@ -32,17 +46,30 @@ class BoxShadowGenerator {
     this.verticalReference.value = this.verticalRange.value;
     this.blurReference.value = this.blurRange.value;
     this.spreadReference.value = this.spreadRange.value;
+    this.shadowColorReference.value = this.shadowColor.value;
+    this.boxColorReference.value = this.boxColor.value;
+    this.opacityReference.value = this.opacityRange.value;
+    this.insetCheckbox.checked = false;
 
     this.applyBoxShadow();
     this.showRule();
   }
 
   applyBoxShadow() {
-    this.previewBox.style.boxShadow = `${this.horizontalReference.value}px 
+    const inset = this.insetCheckbox.checked ? "inset" : "";
+
+    const rgba = `rgba(${this.hexToRgb(this.shadowColorReference.value)}, ${
+      this.opacityReference.value
+    })`;
+    
+    this.previewBox.style.boxShadow = `${inset}
+    ${this.horizontalReference.value}px 
     ${this.verticalReference.value}px 
     ${this.blurReference.value}px 
     ${this.spreadReference.value}px 
-    #000000`;
+    ${rgba}`;
+
+    this.previewBox.style.backgroundColor = this.boxColorReference.value;
 
     this.currentRule = this.previewBox.style.boxShadow;
   }
@@ -57,6 +84,12 @@ class BoxShadowGenerator {
     this.applyBoxShadow();
     this.showRule();
   }
+
+  hexToRgb(hex) {
+    return `${("0x" + hex[1] + hex[2]) | 0}, ${("0x" + hex[3] + hex[4]) | 0}, ${
+      ("0x" + hex[5] + hex[6]) | 0
+    }`;
+  }
 }
 
 // Selecting elements
@@ -68,6 +101,13 @@ const blurRange = document.querySelector("#blur");
 const blurReference = document.querySelector("#blur-reference");
 const spreadRange = document.querySelector("#spread");
 const spreadReference = document.querySelector("#spread-reference");
+const shadowColor = document.querySelector("#shadow-color");
+const shadowColorReference = document.querySelector("#shadow-color-reference");
+const boxColor = document.querySelector("#box-color");
+const boxColorReference = document.querySelector("#box-color-reference");
+const opacityRange = document.querySelector("#opacity");
+const opacityReference = document.querySelector("#opacity-reference");
+const insetCheckbox = document.querySelector("#inset");
 
 const previewBox = document.querySelector("#box");
 
@@ -84,6 +124,13 @@ const boxShadowGenerator = new BoxShadowGenerator(
   blurReference,
   spreadRange,
   spreadReference,
+  shadowColor,
+  shadowColorReference,
+  boxColor,
+  boxColorReference,
+  opacityRange,
+  opacityReference,
+  insetCheckbox,
   previewBox,
   rule,
   webkitRule,
@@ -110,3 +157,10 @@ setupInputEventListeners(horizontalRange, horizontalReference);
 setupInputEventListeners(verticalRange, verticalReference);
 setupInputEventListeners(blurRange, blurReference);
 setupInputEventListeners(spreadRange, spreadReference);
+setupInputEventListeners(shadowColor, shadowColorReference);
+setupInputEventListeners(boxColor, boxColorReference);
+setupInputEventListeners(opacityRange, opacityReference);
+
+insetCheckbox.addEventListener("change", () => {
+  boxShadowGenerator.updateBoxShadow();
+});

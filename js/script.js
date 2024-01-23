9b2c38b0f1a4a35aac60a008f555f96b1a12a0e1
Mateus <mateusc.ramos@outlook.com>
2024-01-23 14:15:39 -0300

    feat: box shadow generator class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,62 @@
-console.log("Hello World!");
+class BoxShadowGenerator {
+  constructor(
+    horizontalRange,
+    horizontalReference,
+    verticalRange,
+    verticalReference,
+    blurRange,
+    blurReference,
+    spreadRange,
+    spreadReference,
+    previewBox,
+    rule,
+    webkitRule,
+    mozRule
+  ) {
+    this.horizontalRange = horizontalRange;
+    this.horizontalReference = horizontalReference;
+    this.verticalRange = verticalRange;
+    this.verticalReference = verticalReference;
+    this.blurRange = blurRange;
+    this.blurReference = blurReference;
+    this.spreadRange = spreadRange;
+    this.spreadReference = spreadReference;
+    this.previewBox = previewBox;
+    this.rule = rule;
+    this.webkitRule = webkitRule;
+    this.mozRule = mozRule;
+  }
+}
+
+// Selecting elements
+const horizontalRange = document.querySelector("#horizontal");
+const horizontalReference = document.querySelector("#horizontal-reference");
+const verticalRange = document.querySelector("#vertical");
+const verticalReference = document.querySelector("#vertical-reference");
+const blurRange = document.querySelector("#blur");
+const blurReference = document.querySelector("#blur-reference");
+const spreadRange = document.querySelector("#spread");
+const spreadReference = document.querySelector("#spread-reference");
+
+const previewBox = document.querySelector("#box");
+
+const rule = document.querySelector("#rule span");
+const webkitRule = document.querySelector("#webkit-rule span");
+const mozRule = document.querySelector("#moz-rule span");
+
+const boxShadowGenerator = new BoxShadowGenerator(
+  horizontalRange,
+  horizontalReference,
+  verticalRange,
+  verticalReference,
+  blurRange,
+  blurReference,
+  spreadRange,
+  spreadReference,
+  previewBox,
+  rule,
+  webkitRule,
+  mozRule
+);
+
+// Event listeners
